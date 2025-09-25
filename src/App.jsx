03ea115c639b835675154e281a8de9d405c2bb4b
rubayet2027 <u2204002@student.cuet.ac.bd>
2025-09-25 16:16:26 +0600

    React Files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import Countries from './Components/countries/countries';
+
+function App() {
+  
+  return (
+    <Countries>
+      
+    </Countries>
+  );
+}
+
+
+export default App;
---
+++ src/Components/countries/countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <p>Welcome </p> <p> to</p> <p>React Worlds</p>
+        </div>
+    );
+};
+
+export default Countries;
